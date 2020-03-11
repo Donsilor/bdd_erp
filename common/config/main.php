d463ab5c30190255d1cd12e7dbf1e393967b30ff
gaopeng <peng.gao@bddco.com>
2020-03-11 18:47:12 +0800

    base

--- common/config/main.php
+++ common/config/main.php
@@ -42,7 +42,7 @@ return [
             'class' => 'yii\redis\Connection',
             'hostname' => '127.0.0.1',
             'port' => 6379,
-            'database' => 0,
+            'database' => 3,
         ],
         /** ------ 网站碎片管理 ------ **/
         'debris' => [
