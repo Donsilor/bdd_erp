f60bec73870175d9b8dc8ca68b509a30f7448477
gaopeng <763429951@qq.com>
2020-03-27 12:03:23 +0800

    框架

---
+++ addons/Supply/backend/controllers/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace addons\Supply\backend\controllers;
+
+use Yii;
+use common\controllers\AddonsController;
+
+/**
+ * 默认控制器
+ *
+ * Class DefaultController
+ * @package addons\Supply\backend\controllers
+ */
+class BaseController extends AddonsController
+{
+    /**
+    * @var string
+    */
+    // public $layout = "@addons/Supply/backend/views/layouts/main";
+}
