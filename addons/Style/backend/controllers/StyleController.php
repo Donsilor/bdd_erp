0cdbabf5616325922264528ad0d105e9bdd1fa57
gaopeng <763429951@qq.com>
2020-04-03 17:35:08 +0800

    fix

--- addons/Style/backend/controllers/StyleController.php
+++ addons/Style/backend/controllers/StyleController.php
@@ -11,6 +11,7 @@ use common\traits\Curd;
 
 use addons\Style\backend\controllers\BaseController;
 use addons\Style\common\models\Style;
+use common\helpers\Url;
 
 /**
 * Style
@@ -95,43 +96,289 @@ class StyleController extends BaseController
     }
     
     /**
-     * 编辑/创建 多语言
+     * 编辑/创建  基础信息
      *
      * @return mixed
      */
-    public function actionEditLang()
+    public function actionEditInfo()
     {
         
         $id = Yii::$app->request->get('id');
-        
-        $cate_id = Yii::$app->request->get('cate_id');
+        $tab = Yii::$app->request->get('tab');
         $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
         $model = $this->findModel($id);
-
-        $status = $model ? $model->status:0;        
+        
         if ($model->load(Yii::$app->request->post())) {
             
             try{
-                $trans = Yii::$app->trans->beginTransaction();              
+                $trans = Yii::$app->trans->beginTransaction();
                 if(false === $model->save()){
                     throw new Exception($this->getError($model));
-                } 
-                
-                $this->editLang($model);
-                
-                $trans->commit();                
+                }
+                $trans->commit();
             }catch (Exception $e){
                 $trans->rollBack();
                 return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
             }
-            
-            //商品更新
-            //\Yii::$app->services->goods->syncStyleToGoods($model->id);
             return $this->message("保存成功", $this->redirect($returnUrl), 'success');
         }
         return $this->render($this->action->id, [
                 'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
         ]);
+    }
+    /**
+     * 编辑-款式属性
+     *
+     * @return mixed
+     */
+    public function actionEditAttr()
+    {    
+        
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab');
+        $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
+        $model = $this->findModel($id);
+        
+        if ($model->load(Yii::$app->request->post())) {
+            
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if(false === $model->save()){
+                    throw new Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (Exception $e){
+                $trans->rollBack();
+                return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
+            }
+            return $this->message("保存成功", $this->redirect($returnUrl), 'success');
+        }
+        return $this->render($this->action->id, [
+                'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
+        ]);
+    }
+    /**
+     * 编辑-款式商品
+     *
+     * @return mixed
+     */
+    public function actionEditGoods()
+    {
+        
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab');
+        $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
+        $model = $this->findModel($id);
+        
+        if ($model->load(Yii::$app->request->post())) {
+            
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if(false === $model->save()){
+                    throw new Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (Exception $e){
+                $trans->rollBack();
+                return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
+            }
+            return $this->message("保存成功", $this->redirect($returnUrl), 'success');
+        }
+        return $this->render($this->action->id, [
+                'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
+        ]);
+    }
+    /**
+     * 编辑-属性模块
+     *
+     * @return mixed
+     */
+    public function actionEditStone()
+    {
+        
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab');
+        $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
+        $model = $this->findModel($id);
+        
+        if ($model->load(Yii::$app->request->post())) {
+            
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if(false === $model->save()){
+                    throw new Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (Exception $e){
+                $trans->rollBack();
+                return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
+            }
+            return $this->message("保存成功", $this->redirect($returnUrl), 'success');
+        }
+        return $this->render($this->action->id, [
+                'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
+        ]);
+    }
+    /**
+     * 编辑-工厂模块
+     *
+     * @return mixed
+     */
+    public function actionEditFactory()
+    {
+        
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab');
+        $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
+        $model = $this->findModel($id);
+        
+        if ($model->load(Yii::$app->request->post())) {
+            
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if(false === $model->save()){
+                    throw new Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (Exception $e){
+                $trans->rollBack();
+                return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
+            }
+            return $this->message("保存成功", $this->redirect($returnUrl), 'success');
+        }
+        return $this->render($this->action->id, [
+                'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
+        ]);
+    }
+    /**
+     * 编辑-工费
+     *
+     * @return mixed
+     */
+    public function actionEditFactoryFee()
+    {
+        
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab');
+        $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
+        $model = $this->findModel($id);
+        
+        if ($model->load(Yii::$app->request->post())) {
+            
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if(false === $model->save()){
+                    throw new Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (Exception $e){
+                $trans->rollBack();
+                return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
+            }
+            return $this->message("保存成功", $this->redirect($returnUrl), 'success');
+        }
+        return $this->render($this->action->id, [
+                'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
+        ]);
+    }
+    /**
+     * 编辑-工费
+     *
+     * @return mixed
+     */
+    public function actionEditImages()
+    {
+        
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab');
+        $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post())) {
+            
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if(false === $model->save()){
+                    throw new Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (Exception $e){
+                $trans->rollBack();
+                return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
+            }
+            return $this->message("保存成功", $this->redirect($returnUrl), 'success');
+        }
+        return $this->render($this->action->id, [
+                'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
+        ]);
+    }
+    /**
+     * 款式日志
+     *
+     * @return mixed
+     */
+    public function actionLogs()
+    {
+        
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab');
+        $returnUrl = Yii::$app->request->get('returnUrl',['index','cate_id'=>$cate_id]);
+        $model = $this->findModel($id);
+        
+        if ($model->load(Yii::$app->request->post())) {
+            
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if(false === $model->save()){
+                    throw new Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (Exception $e){
+                $trans->rollBack();
+                return $this->message("保存失败:". $e->getMessage(), $this->redirect([$this->action->id,'id'=>$model->id,'cate_id'=>$cate_id]), 'error');
+            }
+            return $this->message("保存成功", $this->redirect($returnUrl), 'success');
+        }
+        return $this->render($this->action->id, [
+                'model' => $model,
+                'tabList'=>$this->editTabList(),
+                'tab'=>$tab
+        ]);
+    }
+    /**
+     * 款式编辑 tab
+     * @param string $mod
+     * @return string[][]|boolean[][]
+     */
+    private function editTabList()
+    {
+        $id = \Yii::$app->request->get('id');
+        $tab_list = [
+                1=>['name'=>'基础信息','url'=>Url::to(['edit-info','id'=>$id,'tab'=>1])],
+                2=>['name'=>'款式属性','url'=>Url::to(['edit-attr','id'=>$id,'tab'=>2])],
+                3=>['name'=>'款式规格','url'=>Url::to(['edit-goods','id'=>$id,'tab'=>3])],
+                4=>['name'=>'石头信息','url'=>Url::to(['edit-stone','id'=>$id,'tab'=>4])],
+                5=>['name'=>'工厂信息','url'=>Url::to(['edit-factory','id'=>$id,'tab'=>5])],
+                6=>['name'=>'工费信息','url'=>Url::to(['edit-factory-fee','id'=>$id,'tab'=>6])],
+                7=>['name'=>'款式图片','url'=>Url::to(['edit-images','id'=>$id,'tab'=>7])],
+                8=>['name'=>'日志信息','url'=>Url::to(['logs','id'=>$id,'tab'=>8])]
+        ];
+        
+        return $tab_list;
     }
    
 }
---
+++ addons/Style/backend/views/style/logs.php
@@ -0,0 +1,103 @@
+<?php
+
+use common\helpers\Html;
+use yii\widgets\ActiveForm;
+use common\widgets\langbox\LangBox;
+use yii\base\Widget;
+
+use common\helpers\Url;
+use common\enums\StatusEnum;
+use common\helpers\AmountHelper;
+use common\enums\AreaEnum;
+use addons\style\common\models\Goods;
+
+/* @var $this yii\web\View */
+/* @var $model addons\style\common\models\Style */
+/* @var $form yii\widgets\ActiveForm */
+
+$this->title = Yii::t('goods', 'Style');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('goods', 'Styles'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<?php $form = ActiveForm::begin([
+        'id' => $model->formName(),
+        'enableAjaxValidation' => true,
+        'validationUrl' => Url::to(['ajax-edit', 'id' => $model->id]),       
+]); ?>
+<div class="box-body nav-tabs-custom">
+     <h2 class="page-header">款式发布</h2>
+     <?php echo Html::menuTab($tabList,$tab)?>
+     <div class="tab-content">     
+       <div class="row nav-tabs-custom tab-pane tab0 active">
+            <ul class="nav nav-tabs pull-right">
+              <li class="pull-left header"><i class="fa fa-th"></i> <?= $tabList[$tab]['name']??'';?></li>
+            </ul>
+            <div class="box-body col-sm-10" style="margin-left:9px">
+       			<div class="row">
+                    <div class="col-lg-6"><?= $form->field($model, 'style_sn')->textInput(['disabled'=>$model->isNewRecord?null:'disabled'])?></div>
+                    <div class="col-lg-6"><?= $form->field($model, 'style_name')->textInput()?></div>
+                </div>
+    			<div class="row">
+                    <div class="col-lg-6">
+                    <?= $form->field($model, 'style_cate_id')->widget(\kartik\select2\Select2::class, [
+                        'data' => \Yii::$app->styleService->styleCate->getGrpDropDown(),
+                        'options' => ['placeholder' => '请选择'],
+                        'pluginOptions' => [
+                            'allowClear' => false,
+                            'disabled'=>$model->isNewRecord?null:'disabled'
+                        ],
+                    ]);?>
+                    </div>
+                    <div class="col-lg-6">
+                    <?= $form->field($model, 'product_type_id')->widget(\kartik\select2\Select2::class, [
+                        'data' => \Yii::$app->styleService->productType->getGrpDropDown(),
+                        'options' => ['placeholder' => '请选择'],
+                        'pluginOptions' => [
+                            'allowClear' => false,
+                            'disabled'=>$model->isNewRecord?null:'disabled'
+                        ],
+                    ]);?>                
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                    <?= $form->field($model, 'style_source_id')->widget(\kartik\select2\Select2::class, [
+                        'data' => \Yii::$app->styleService->styleSource->getdropDown(),
+                        'options' => ['placeholder' => '请选择'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ]);?>
+                    </div>
+                    <div class="col-lg-6">
+                    <?= $form->field($model, 'style_channel_id')->widget(\kartik\select2\Select2::class, [
+                        'data' => \Yii::$app->styleService->styleChannel->getdropDown(),
+                        'options' => ['placeholder' => '请选择'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ]);?>                
+                    </div>
+                </div>
+       		    <div class="row">
+                    <div class="col-lg-6"><?= $form->field($model, 'style_sex')->radioList(\addons\Style\common\enums\StyleSexEnum::getMap())?></div>
+                    <div class="col-lg-6"><?= $form->field($model, 'is_made')->radioList(\common\enums\ConfirmEnum::getMap())?></div>
+                </div>
+                <div class="row">
+                <?= $form->field($model, 'remark')->textArea(['options'=>['maxlength' => true]])?>
+                </div>
+      
+         </div>
+        <!-- ./box-body -->
+      </div>          
+      
+    </div>
+    <div class="modal-footer">
+        <div class="col-sm-10 text-center">
+            <button class="btn btn-primary" type="submit">保存</button>
+            <span class="btn btn-white" onclick="history.go(-1)">返回</span>
+        </div>
+	</div>
+</div>
+<?php ActiveForm::end(); ?>
