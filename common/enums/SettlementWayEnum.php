45980f313f4acdad7e69f18548c38149e6771a86
hexiaowei <xiaowei.he@bddco.com>
2020-05-12 09:09:31 +0800

    Squashed commit of the following:
    commit a2dc2852da109750ab8f9f1a09ee264f76bb6c7c
    Author: hexiaowei <xiaowei.he@bddco.com>
    Date:   Tue May 12 09:08:05 2020 +0800
    
        供应商管理优化
    
    commit e5a30e26d2a6d083cfc6a947282b67da691c4538
    Author: hexiaowei <xiaowei.he@bddco.com>
    Date:   Mon May 11 21:37:14 2020 +0800
    
        更新
    
    commit b9a1b819bf5538c8822db42fdd1c55c9fbad957e
    Author: hexiaowei <xiaowei.he@bddco.com>
    Date:   Mon May 11 19:01:05 2020 +0800
    
        view 继承
    
    commit 5f4fb6ee5b3572406c0c798f0ab48741db3f84ae
    Author: hexiaowei <xiaowei.he@bddco.com>
    Date:   Mon May 11 18:46:32 2020 +0800
    
        供应商列表

---
+++ common/enums/SettlementWayEnum.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace common\enums;
+
+/**
+ * 结算方式
+ *
+ * Class SettlementWayEnum
+ * @package common\enums
+ * @author jianyan74 <751393839@qq.com>
+ */
+class SettlementWayEnum extends BaseEnum
+{
+	const TRANSFER = 1;
+	const CASH = 2;
+    const CHEQUE = 3;
+	
+	/**
+	 * @return array
+	 */
+	public static function getMap(): array
+	{
+		return [
+				self::TRANSFER => '转账',
+				self::CASH => '现金',
+                self::CHEQUE => '支票',
+		];
+	}
+}
