770368dab557ff0606d027073b2993007ae351f9
hexiaowei <xiaowei.he@bddco.com>
2020-05-12 10:57:50 +0800

    供应商审核

--- addons/Supply/backend/views/supplier/index.php
+++ addons/Supply/backend/views/supplier/index.php
@@ -132,6 +132,18 @@ $this->params['breadcrumbs'][] = $this->title;
                 'headerOptions' => ['width'=>'160'],
             ],
             [
+                'attribute' => 'audit_status',
+                'format' => 'raw',
+                'headerOptions' => ['class' => 'col-md-1', 'width'=>'60'],
+                'value' => function ($model){
+                    return \common\enums\AuditStatusEnum::getValue($model->audit_status);
+                },
+                'filter' => Html::activeDropDownList($searchModel, 'audit_status',\common\enums\AuditStatusEnum::getMap(), [
+                    'prompt' => '全部',
+                    'class' => 'form-control',
+                ]),
+            ],
+            [
                 'attribute' => 'status',
                 'format' => 'raw',
                 'headerOptions' => ['class' => 'col-md-1', 'width'=>'60'],
@@ -141,28 +153,36 @@ $this->params['breadcrumbs'][] = $this->title;
                 'filter' => Html::activeDropDownList($searchModel, 'status',\common\enums\StatusEnum::getMap(), [
                     'prompt' => '全部',
                     'class' => 'form-control',
-
                 ]),
             ],
             [
                 'class' => 'yii\grid\ActionColumn',
                 'header' => '操作',
-                'template' => '{edit} {status}',
+                'template' => '{edit} {audit} {status}',
                 'buttons' => [
                 'edit' => function($url, $model, $key){
                         return Html::edit(['edit-lang', 'id' => $model->id, 'returnUrl' => Url::getReturnUrl()]);
-                },
-               'status' => function($url, $model, $key){
+                    },
+                'audit' => function($url, $model, $key){
+                        if($model->audit_status != 1){
+                            return Html::edit(['ajax-audit','id'=>$model->id], '审核', [
+                                'class'=>'btn btn-success btn-sm',
+                                'data-toggle' => 'modal',
+                                'data-target' => '#ajaxModal',
+                            ]);
+                        }
+                    },
+                 'status' => function($url, $model, $key){
                         return Html::status($model['status']);
-                  },
-                    /*'delete' => function($url, $model, $key){
-                            return Html::delete(['delete', 'id' => $model->id]);
                     },
-                    'view'=> function($url, $model, $key){
-                        return Html::a('预览', \Yii::$app->params['frontBaseUrl'].'/diamond-details/'.$model->id.'?goodId='.$model->id.'&backend=1',['class'=>'btn btn-info btn-sm','target'=>'_blank']);
+                /*'delete' => function($url, $model, $key){
+                        return Html::delete(['delete', 'id' => $model->id]);
                     },
-                    'show_log' => function($url, $model, $key){
-                        return Html::linkButton(['goods-log/index','id' => $model->id, 'type_id' => $model->type_id, 'returnUrl' => Url::getReturnUrl()], '日志');
+                'view'=> function($url, $model, $key){
+                    return Html::a('预览', \Yii::$app->params['frontBaseUrl'].'/diamond-details/'.$model->id.'?goodId='.$model->id.'&backend=1',['class'=>'btn btn-info btn-sm','target'=>'_blank']);
+                    },
+                'show_log' => function($url, $model, $key){
+                    return Html::linkButton(['goods-log/index','id' => $model->id, 'type_id' => $model->type_id, 'returnUrl' => Url::getReturnUrl()], '日志');
                     },*/
                 ]
             ]
