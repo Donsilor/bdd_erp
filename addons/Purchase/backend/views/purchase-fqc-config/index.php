dde1b8dd7faefee53d78869439fd30562d3b119d
hexiaowei <xiaowei.he@bddco.com>
2020-05-15 18:37:57 +0800

    质检未过原因

---
+++ addons/Purchase/backend/views/purchase-fqc-config/index.php
@@ -0,0 +1,112 @@
+<?php
+use common\helpers\Html;
+use jianyan\treegrid\TreeGrid;
+use yii\widgets\ActiveForm;
+use common\helpers\Url;
+
+$this->title = '产品分类';
+$this->params['breadcrumbs'][] = ['label' => $this->title];
+
+?>
+
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title"><?= $this->title; ?></h3>
+                <div class="box-tools">
+                    <?= Html::create(['ajax-edit'], '创建', [
+                        'data-toggle' => 'modal',
+                        'data-target' => '#ajaxModalLg',
+                    ])?>
+                </div>
+            </div>
+
+            <div class="box-body table-responsive">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <?php $form = ActiveForm::begin([
+                            'action' => Url::to(['index']),
+                            'method' => 'get',
+                        ]); ?>
+
+                        <div class="col-sm-4">
+                            <div class="input-group m-b">
+                                <input type="text" class="form-control" name="title" placeholder="标题或者ID" value="<?= Yii::$app->request->get('title') ?>"/>
+                                <span class="input-group-btn"><button class="btn btn-white"><i class="fa fa-search"></i> 搜索</button></span>
+                            </div>
+                        </div>
+                        <?php ActiveForm::end(); ?>
+                    </div>
+                </div>
+                <?= TreeGrid::widget([
+                    'dataProvider' => $dataProvider,
+                    'keyColumnName' => 'id',
+                    'parentColumnName' => 'pid',
+                    'parentRootValue' => '0', //first parentId value
+                    'pluginOptions' => [
+//                        'initialState' => 'collapsed',
+                    ],
+                    'options' => ['class' => 'table table-hover'],
+
+                    'columns' => [
+
+
+
+                        [
+                            'attribute' => 'name',
+                            'format' => 'raw',
+                            'value' => function ($model, $key, $index, $column){
+                                $str = Html::tag('span', $model->name, [
+                                    'class' => 'm-l-sm'
+                                ]);
+                                $str .= Html::a(' <i class="icon ion-android-add-circle"></i>', ['ajax-edit', 'pid' => $model['id']], [
+                                    'data-toggle' => 'modal',
+                                    'data-target' => '#ajaxModal',
+                                ]);
+                                return $str;
+                            },
+
+                           ],
+
+                        [
+                            'attribute'=>'id',
+                            'value'=> 'id',
+                            'headerOptions'=>['style'=>'width:50px;'],
+
+                        ],
+                        [
+                            'attribute' => 'sort',
+                            'format' => 'raw',
+                            'headerOptions' => ['class' => 'col-md-1'],
+                            'value' => function ($model, $key, $index, $column){
+                                return  Html::sort($model->sort);
+                            }
+                        ],
+                        [
+                            'header' => "操作",
+                            'class' => 'yii\grid\ActionColumn',
+                            'template'=> '{edit} {status}',
+                            'buttons' => [
+                                'edit' => function ($url, $model, $key) {
+                                    return Html::edit(['ajax-edit','id' => $model->id], '编辑', [
+                                        'data-toggle' => 'modal',
+                                        'data-target' => '#ajaxModalLg',
+                                    ]);
+                                },
+                                'status' => function ($url, $model, $key) {
+                                    return Html::status($model->status);
+                                },
+                                'delete' => function ($url, $model, $key) {
+                                    return Html::delete(['delete','id' => $model->id]);
+                                },
+                            ],
+                        ],
+                    ]
+                ]); ?>
+
+            </div>
+
+        </div>
+    </div>
+</div>
--- addons/Purchase/services/Application.php
+++ addons/Purchase/services/Application.php
@@ -12,6 +12,7 @@ use common\components\Service;
  * @property \addons\Purchase\services\PurchaseGoodsService $purchaseGoods 采购明细
  * @property \addons\Purchase\services\purchaseReceiptService $purchaseReceipt 采购收货单
  * @property \addons\Purchase\services\purchaseDefectiveService $purchaseDefective 采购收货单
+ * @property \addons\Purchase\services\purchaseFqcConfigService $purchaseFqcConfig 质检未过原因
  * @var array
  */
 class Application extends Service
@@ -23,5 +24,6 @@ class Application extends Service
             'purchaseGoods' => 'addons\Purchase\services\PurchaseGoodsService',
             'purchaseReceipt' => 'addons\Purchase\services\PurchaseReceiptService',
             'purchaseDefective' => 'addons\Purchase\services\purchaseDefectiveService',
+            'purchaseFqcConfig' => 'addons\Purchase\services\purchaseFqcConfigService',
     ];
 }
