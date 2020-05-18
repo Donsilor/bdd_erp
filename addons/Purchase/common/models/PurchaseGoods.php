765a508e3c1c42859e5e95ce722fa465947abcca
lrz0829 <502034356@qq.com>
2020-05-18 20:21:59 +0800

    1

--- addons/Purchase/common/models/PurchaseGoods.php
+++ addons/Purchase/common/models/PurchaseGoods.php
@@ -13,11 +13,12 @@ use addons\Supply\common\models\Produce;
  * @property int $id ID
  * @property int $purchase_id 采购单ID
  * @property int $goods_type 商品类型 1款号 2起版号
- * @property int $style_id 款号id
- * @property string $style_sn 款式编号
- * @property string $goods_sn 商品编号
- * @property string $qiban_sn 起版编号
- * @property int $goods_name  商品名称
+ * @property string $goods_name 商品名称
+ * @property string $goods_sn 款号/起版号
+ * @property int $style_id 商品id
+ * @property string $style_sn 商品编号
+ * @property string $qiban_sn
+ * @property int $qiban_type 起版类型 0非起版 1有款起版 2无款起版
  * @property int $product_type_id 产品线
  * @property int $style_cate_id 款式分类
  * @property int $style_sex 款式性别
@@ -26,10 +27,25 @@ use addons\Supply\common\models\Produce;
  * @property int $goods_num 商品数量
  * @property int $produce_id 布产ID
  * @property int $is_apply 是否申请修改
- * @property int $apply_info 申请修改数据
+ * @property string $apply_info
  * @property int $status 状态： -1已删除 0禁用 1启用
+ * @property string $remark 备注
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
+ * @property string $main_stone_price 主石价格(元/颗)
+ * @property string $second_stone_price 副石单价
+ * @property string $stone_info 石料信息
+ * @property double $gold_loss 金损
+ * @property string $gold_price 金价
+ * @property string $gold_cost_price 单件银(金)额
+ * @property string $parts_info 配件信息
+ * @property string $gong_fee 工费/件
+ * @property string $xiangqian_fee 镶石费/件
+ * @property string $total_gong_fee 工费总额/件
+ * @property string $gaitu_fee 改图费
+ * @property string $penla_fee 喷蜡费
+ * @property string $danjian_cost 单件额
+ * @property string $factory_cost_price 单件工厂金额
  */
 class PurchaseGoods extends BaseModel
 {
@@ -47,12 +63,13 @@ class PurchaseGoods extends BaseModel
     public function rules()
     {
         return [
-            [['goods_sn','purchase_id', 'goods_type','goods_name','cost_price','product_type_id','style_cate_id','goods_num','jintuo_type'], 'required'],
-            [['purchase_id', 'goods_type','qiban_type','jintuo_type','is_apply', 'style_id', 'product_type_id', 'style_cate_id', 'style_sex', 'goods_num', 'produce_id', 'status', 'created_at', 'updated_at'], 'integer'],
-            [['cost_price'], 'number'],
-            [['style_sn','qiban_sn','goods_sn'], 'string', 'max' => 30],
-            [['goods_name','remark'], 'string', 'max' => 255],
-            [['apply_info'], 'safe'],
+            [['purchase_id', 'goods_type'], 'required'],
+            [['purchase_id', 'goods_type', 'style_id', 'qiban_type', 'product_type_id', 'style_cate_id', 'style_sex', 'jintuo_type', 'goods_num', 'produce_id', 'is_apply', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['cost_price', 'main_stone_price', 'second_stone_price', 'gold_loss', 'gold_price', 'gold_cost_price', 'gong_fee', 'xiangqian_fee', 'total_gong_fee', 'gaitu_fee', 'penla_fee', 'danjian_cost', 'factory_cost_price'], 'number'],
+            [['apply_info'], 'string'],
+            [['goods_name', 'remark', 'stone_info', 'parts_info'], 'string', 'max' => 255],
+            [['goods_sn'], 'string', 'max' => 60],
+            [['style_sn', 'qiban_sn'], 'string', 'max' => 30],
         ];
     }
 
@@ -65,12 +82,12 @@ class PurchaseGoods extends BaseModel
             'id' => 'ID',
             'purchase_id' => '采购单ID',
             'goods_type' => '商品类型',
-            'style_id' => '款号id',
-            'goods_sn' => '款号/起版号',
-            'style_sn' => '款号',
-            'qiban_sn' => '起版号',
-            'qiban_type' => '起版类型',
             'goods_name' => '商品名称',
+            'goods_sn' => '款号/起版号',
+            'style_id' => '商品id',
+            'style_sn' => '商品编号',
+            'qiban_sn' => 'Qiban Sn',
+            'qiban_type' => '起版类型',
             'product_type_id' => '产品线',
             'style_cate_id' => '款式分类',
             'style_sex' => '款式性别',
@@ -78,10 +95,26 @@ class PurchaseGoods extends BaseModel
             'cost_price' => '成本价',
             'goods_num' => '商品数量',
             'produce_id' => '布产ID',
-            'remark' => '采购备注',
+            'is_apply' => '是否申请修改',
+            'apply_info' => 'Apply Info',
             'status' => '状态',
+            'remark' => '备注',
             'created_at' => '创建时间',
             'updated_at' => '更新时间',
+            'main_stone_price' => '主石价格(元/颗)',
+            'second_stone_price' => '副石单价',
+            'stone_info' => '石料信息',
+            'gold_loss' => '金损',
+            'gold_price' => '金价',
+            'gold_cost_price' => '单件银(金)额',
+            'parts_info' => '配件信息',
+            'gong_fee' => '工费/件',
+            'xiangqian_fee' => '镶石费/件',
+            'total_gong_fee' => '工费总额/件',
+            'gaitu_fee' => '改图费',
+            'penla_fee' => '喷蜡费',
+            'danjian_cost' => '单件额',
+            'factory_cost_price' => '单件工厂金额',
         ];
     }
     
--- addons/Supply/backend/views/produce/view.php
+++ addons/Supply/backend/views/produce/view.php
@@ -21,174 +21,157 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="row">
      <div class="col-xs-12">
          <div class="box" style="margin: 0px;">
-            <div class="box-header" style="padding-top: 0px;">
-                <h3 class="box-title"><i class="fa fa-bars"></i> 基本信息</h3>
-                <div class="box-tools" style="top: 0px;">
-                    <?php
-                    $buttonHtml = '';
-                    switch ($model->bc_status){
+             <div class="col-xs-6" style="padding:0px;margin-top: 0px;">
+                 <div class="box">
+                     <div class="box-body table-responsive" style="padding-top: 0px;padding-bottom: 0px;">
+                         <table class="table table-hover">
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('produce_sn') ?>：</td>
+                                 <td><?= $model->produce_sn ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('from_type') ?>：</td>
+                                 <td><?= \addons\Supply\common\enums\FromTypeEnum::getValue($model->from_type) ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('bc_status') ?>：</td>
+                                 <td><?= \addons\Supply\common\enums\BuChanEnum::getValue($model->bc_status) ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('from_order_sn') ?>：</td>
+                                 <td><?= $model->from_order_sn ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('from_detail_id') ?>：</td>
+                                 <td><?= $model->from_type == 2 ? $model->purchaseGoods->goods_name : '客订单' ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('goods_num') ?>：</td>
+                                 <td><?= $model->goods_num ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('supplier_id') ?>：</td>
+                                 <td><?= $model->supplier ?  $model->supplier->supplier_name : '' ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('follower_id') ?>：</td>
+                                 <td><?=  $model->follower ?  $model->follower->member_name : '' ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('customer') ?>：</td>
+                                 <td><?= $model->customer ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('factory_order_time') ?>：</td>
+                                 <td><?= \Yii::$app->formatter->asDatetime($model->factory_order_time) ?></td>
+                             </tr>
 
-                        //确认分配
-                        case BuChanEnum::TO_CONFIRMED:
-                            $buttonHtml .= Html::edit(['to-confirmed','id'=>$model->id ,'returnUrl'=>$returnUrl], '确认分配', [
-                                'class'=>'btn btn-info btn-xs',
-                                'style'=>"margin-left:5px",
-                                'onclick' => 'rfTwiceAffirm(this,"确认分配","确定操作吗？");return false;',
-
-                            ]);
-                        //初始化
-                        case BuChanEnum::INITIALIZATION:
-                            $buttonHtml .= Html::edit(['to-factory','id'=>$model->id ,'returnUrl'=>$returnUrl], '分配工厂', [
-                                'class'=>'btn btn-primary btn-xs',
-                                'style'=>"margin-left:5px",
-                                'data-toggle' => 'modal',
-                                'data-target' => '#ajaxModal',
-                            ]);
-                            break;
-                        //已分配
-                        case BuChanEnum::ASSIGNED:
-                            $buttonHtml .= Html::edit(['to-produce','id'=>$model->id ,'returnUrl'=>$returnUrl], '开始生产', [
-                                'class'=>'btn btn-danger btn-xs',
-                                'style'=>"margin-left:5px",
-                                'onclick' => 'rfTwiceAffirm(this,"开始生产","确定操作吗？");return false;',
-
-                            ]);
-                            break;
-                        //生产中
-                        case BuChanEnum::IN_PRODUCTION :
-                            ;
-                        //部分出厂
-                        case BuChanEnum::PARTIALLY_SHIPPED:
-                            $buttonHtml .= Html::edit(['produce-shipment','id'=>$model->id ,'returnUrl'=>$returnUrl], '生产出厂', [
-                                'class'=>'btn btn-success btn-xs',
-                                'style'=>"margin-left:5px",
-                                'data-toggle' => 'modal',
-                                'data-target' => '#ajaxModalLg',
-                            ]);
-                            break;
-//                     case BuChanEnum::FACTORY:
-//                         $buttonHtml .= Html::edit(['produce-oqc','id'=>$model->id ,'returnUrl'=>$returnUrl], 'QC质检', [
-//                             'class'=>'btn btn-success btn-sm',
-//                             'style'=>"margin-left:5px",
-//                             'data-toggle' => 'modal',
-//                             'data-target' => '#ajaxModalLg',
-//                         ]);
-//                         break;
-                        default:
-                            $buttonHtml .= '';
-
-                    }
-                    echo $buttonHtml;
-                    ?>
-                </div>
-            </div>
-             <div class="box-body table-responsive">
-                 <div class="col-xs-6" style="margin-top: 0px;">
-                     <div class="box">
-                         <div class="box-body table-responsive" style="padding-top: 0px;padding-bottom: 0px;">
-                             <table class="table table-hover">
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('produce_sn') ?>：</td>
-                                     <td><?= $model->produce_sn ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('from_type') ?>：</td>
-                                     <td><?= \addons\Supply\common\enums\FromTypeEnum::getValue($model->from_type) ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('bc_status') ?>：</td>
-                                     <td><?= \addons\Supply\common\enums\BuChanEnum::getValue($model->bc_status) ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('from_order_sn') ?>：</td>
-                                     <td><?= $model->from_order_sn ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('from_detail_id') ?>：</td>
-                                     <td><?= $model->from_type == 2 ? $model->purchaseGoods->goods_name : '客订单' ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('goods_num') ?>：</td>
-                                     <td><?= $model->goods_num ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('supplier_id') ?>：</td>
-                                     <td><?= $model->supplier ?  $model->supplier->supplier_name : '' ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('follower_id') ?>：</td>
-                                     <td><?=  $model->follower ?  $model->follower->member_name : '' ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('customer') ?>：</td>
-                                     <td><?= $model->customer ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('factory_order_time') ?>：</td>
-                                     <td><?= \Yii::$app->formatter->asDatetime($model->factory_order_time) ?></td>
-                                 </tr>
-
-                             </table>
-                         </div>
+                         </table>
                      </div>
                  </div>
+             </div>
 
-                 <div class="col-xs-6">
-                     <div class="box">
-                         <div class="box-body table-responsive" style="padding-top: 0px;padding-bottom: 0px;">
-                             <table class="table table-hover">
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('style_sn') ?>：</td>
-                                     <td><?= $model->style_sn ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('style_sex') ?>：</td>
-                                     <td><?= \addons\Style\common\enums\StyleSexEnum::getValue($model->style_sex) ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('jintuo_type') ?>：</td>
-                                     <td><?= \addons\Style\common\enums\JintuoTypeEnum::getValue($model->jintuo_type) ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('qiban_sn') ?>：</td>
-                                     <td><?= $model->qiban_sn ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('qiban_type') ?>：</td>
-                                     <td><?= \addons\Style\common\enums\QibanTypeEnum::getValue($model->qiban_type) ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('product_type_id') ?>：</td>
-                                     <td><?= $model->type->name ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('style_cate_id') ?>：</td>
-                                     <td><?= $model->cate->name ?></td>
-                                 </tr>
+             <div class="col-xs-6" style="padding:0px;margin-top: 0px;">
+                 <div class="box">
+                     <div class="box-body table-responsive" style="padding-top: 0px;padding-bottom: 0px;">
+                         <table class="table table-hover">
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('style_sn') ?>：</td>
+                                 <td><?= $model->style_sn ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('style_sex') ?>：</td>
+                                 <td><?= \addons\Style\common\enums\StyleSexEnum::getValue($model->style_sex) ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('jintuo_type') ?>：</td>
+                                 <td><?= \addons\Style\common\enums\JintuoTypeEnum::getValue($model->jintuo_type) ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('qiban_sn') ?>：</td>
+                                 <td><?= $model->qiban_sn ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('qiban_type') ?>：</td>
+                                 <td><?= \addons\Style\common\enums\QibanTypeEnum::getValue($model->qiban_type) ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('product_type_id') ?>：</td>
+                                 <td><?= $model->type->name ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('style_cate_id') ?>：</td>
+                                 <td><?= $model->cate->name ?></td>
+                             </tr>
 
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('created_at') ?>：</td>
-                                     <td><?= \Yii::$app->formatter->asDatetime($model->created_at) ?></td>
-                                 </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('created_at') ?>：</td>
+                                 <td><?= \Yii::$app->formatter->asDatetime($model->created_at) ?></td>
+                             </tr>
 
-                                 <tr>
-                                     <td class="col-xs-1 text-right"><?= $model->getAttributeLabel('factory_distribute_time') ?>：</td>
-                                     <td><?= \Yii::$app->formatter->asDatetime($model->factory_distribute_time) ?></td>
-                                 </tr>
-                                 <tr>
-                                     <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('factory_delivery_time') ?>：</td>
-                                     <td><?= \Yii::$app->formatter->asDatetime($model->factory_delivery_time) ?></td>
-                                 </tr>
+                             <tr>
+                                 <td class="col-xs-1 text-right"><?= $model->getAttributeLabel('factory_distribute_time') ?>：</td>
+                                 <td><?= \Yii::$app->formatter->asDatetime($model->factory_distribute_time) ?></td>
+                             </tr>
+                             <tr>
+                                 <td class="col-xs-2 text-right"><?= $model->getAttributeLabel('factory_delivery_time') ?>：</td>
+                                 <td><?= \Yii::$app->formatter->asDatetime($model->factory_delivery_time) ?></td>
+                             </tr>
 
-                             </table>
-                         </div>
+                         </table>
                      </div>
                  </div>
+             </div>
 
+             <div class="box-footer text-center" >
+                 <?php
+                 $buttonHtml = '';
+                 switch ($model->bc_status){
 
+                     //确认分配
+                     case BuChanEnum::TO_CONFIRMED:
+                         $buttonHtml .= Html::edit(['to-confirmed','id'=>$model->id ,'returnUrl'=>$returnUrl], '确认分配', [
+                             'class'=>'btn btn-info btn-ms',
+                             'style'=>"margin-left:5px",
+                             'onclick' => 'rfTwiceAffirm(this,"确认分配","确定操作吗？");return false;',
 
+                         ]);
+                     //初始化
+                     case BuChanEnum::INITIALIZATION:
+                         $buttonHtml .= Html::edit(['to-factory','id'=>$model->id ,'returnUrl'=>$returnUrl], '分配工厂', [
+                             'class'=>'btn btn-primary btn-ms',
+                             'style'=>"margin-left:5px",
+                             'data-toggle' => 'modal',
+                             'data-target' => '#ajaxModal',
+                         ]);
+                         break;
+                     //已分配
+                     case BuChanEnum::ASSIGNED:
+                         $buttonHtml .= Html::edit(['to-produce','id'=>$model->id ,'returnUrl'=>$returnUrl], '开始生产', [
+                             'class'=>'btn btn-danger btn-ms',
+                             'style'=>"margin-left:5px",
+                             'onclick' => 'rfTwiceAffirm(this,"开始生产","确定操作吗？");return false;',
 
+                         ]);
+                         break;
+                     //生产中
+                     case BuChanEnum::IN_PRODUCTION :
+                         ;
+                     //部分出厂
+                     case BuChanEnum::PARTIALLY_SHIPPED:
+                         $buttonHtml .= Html::edit(['produce-shipment','id'=>$model->id ,'returnUrl'=>$returnUrl], '生产出厂', [
+                             'class'=>'btn btn-success btn-ms',
+                             'style'=>"margin-left:5px",
+                             'data-toggle' => 'modal',
+                             'data-target' => '#ajaxModalLg',
+                         ]);
+                         break;
+                     default:
+                         $buttonHtml .= '';
 
+                 }
+                 echo $buttonHtml;
+                 ?>
              </div>
          </div>
      </div>
