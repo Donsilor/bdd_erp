d1931a64dbbafb8081c7213b3b6945bff0355aae
hexiaowei <xiaowei.he@bddco.com>
2020-05-25 12:00:43 +0800

    日志

--- addons/Warehouse/services/WarehouseBillLService.php
+++ addons/Warehouse/services/WarehouseBillLService.php
@@ -32,7 +32,7 @@ class WarehouseBillLService extends Service
             1=>['name'=>'单据详情','url'=>Url::to(['warehouse-bill-l/view','id'=>$bill_id,'tab'=>1,'returnUrl'=>$returnUrl])],
             2=>['name'=>'单据明细','url'=>Url::to(['warehouse-bill-l-goods/index','bill_id'=>$bill_id,'tab'=>2,'returnUrl'=>$returnUrl])],
             3=>['name'=>'结算商信息','url'=>Url::to(['warehouse-bill-pay/index','bill_id'=>$bill_id,'tab'=>3,'returnUrl'=>$returnUrl])],
-            4=>['name'=>'日志信息','url'=>Url::to(['warehouse-bill-l-log/index','bill_id'=>$bill_id,'tab'=>4,'returnUrl'=>$returnUrl])],
+            4=>['name'=>'日志信息','url'=>Url::to(['warehouse-bill-log/index','bill_id'=>$bill_id,'tab'=>4,'returnUrl'=>$returnUrl])],
         ];
     }
 
