721b8f26bad894f01036fddd67f9bf1e6a4c4dca
gaopeng <763429951@qq.com>
2020-05-27 14:58:45 +0800

    fix

--- common/enums/AuditStatusEnum.php
+++ common/enums/AuditStatusEnum.php
@@ -21,7 +21,7 @@ class AuditStatusEnum extends BaseEnum
     public static function getMap(): array
     {
         return [
-                self::PENDING => '待审核',
+                self::PENDING => '未审核',
                 self::PASS => '审核通过',                
                 self::UNPASS => '不通过',
                 //self::CLOSE => '已关闭',
