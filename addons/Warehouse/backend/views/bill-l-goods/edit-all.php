e0958c4467dc8835a923afcf8bd99175c80a0e07
hexiaowei <xiaowei.he@bddco.com>
2020-06-08 20:50:39 +0800

    fix

--- addons/Warehouse/backend/views/bill-l-goods/edit-all.php
+++ addons/Warehouse/backend/views/bill-l-goods/edit-all.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="box-tools" style="float:right;margin-top:-40px; margin-right: 20px;">
         <?php
         if($bill->bill_status == \addons\Warehouse\common\enums\BillStatusEnum::SAVE) {
-            echo Html::a('返回列表', ['warehouse-bill-l-goods/index', 'bill_id' => $bill->id], ['class' => 'btn btn-white btn-xs']);
+            echo Html::a('返回列表', ['bill-l-goods/index', 'bill_id' => $bill->id], ['class' => 'btn btn-white btn-xs']);
         }
         ?>
     </div>
