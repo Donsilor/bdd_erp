211a22a6d369148fc8181bbac0acadc5d67d0bff
hexiaowei <xiaowei.he@bddco.com>
2020-06-13 15:40:07 +0800

    fix

---
+++ addons/Style/backend/controllers/StoneStyleController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace addons\Style\backend\controllers;
+
+use Yii;
+use common\traits\Curd;
+use common\models\base\SearchModel;
+use addons\Style\common\models\StyleStoneStyle;
+use common\enums\AuditStatusEnum;
+use common\enums\StatusEnum;
+
+/**
+ * StoneStyleController implements the CRUD actions for StyleStoneStyle model.
+ */
+class StoneStyleController extends BaseController
+{
+    use Curd;
+    public $modelClass = StyleStoneStyle::class;
+    /**
+     * Lists all StyleStoneStyle models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new SearchModel([
+            'model' => $this->modelClass,
+            'scenario' => 'default',
+            'partialMatchAttributes' => [], // 模糊查询
+            'defaultOrder' => [
+                'id' => SORT_DESC
+            ],
+            'pageSize' => $this->pageSize,
+            'relations' => [
+                'creator' => ['username'],
+                'auditor' => ['username'],
+            ]
+        ]);
+
+        $dataProvider = $searchModel
+            ->search(Yii::$app->request->queryParams,['created_at']);
+
+        $created_at = $searchModel->created_at;
+        if (!empty($created_at)) {
+            $dataProvider->query->andFilterWhere(['>=',StyleStoneStyle::tableName().'.created_at', strtotime(explode('/', $created_at)[0])]);//起始时间
+            $dataProvider->query->andFilterWhere(['<',StyleStoneStyle::tableName().'.created_at', (strtotime(explode('/', $created_at)[1]) + 86400)] );//结束时间
+        }
+
+        $dataProvider = $searchModel
+            ->search(Yii::$app->request->queryParams,['audit_time']);
+
+        $audit_time = $searchModel->audit_time;
+        if (!empty($audit_time)) {
+            $dataProvider->query->andFilterWhere(['>=',StyleStoneStyle::tableName().'.audit_time', strtotime(explode('/', $audit_time)[0])]);//起始时间
+            $dataProvider->query->andFilterWhere(['<',StyleStoneStyle::tableName().'.audit_time', (strtotime(explode('/', $audit_time)[1]) + 86400)] );//结束时间
+        }
+
+        $dataProvider->query->andWhere(['>',StyleStoneStyle::tableName().'.status',-1]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
+        ]);
+    }
+    /**
+     * @return mixed
+     * 提交审核
+     */
+    public function actionAjaxApply(){
+        $id = \Yii::$app->request->get('id');
+        $model = $this->findModel($id);
+        $model = $model ?? new StyleStoneStyle();
+        if($model->audit_status != AuditStatusEnum::SAVE){
+            return $this->message('不是保存状态', $this->redirect(\Yii::$app->request->referrer), 'error');
+        }
+        $model->audit_status = AuditStatusEnum::PENDING;
+        if(false === $model->save()){
+            return $this->message($this->getError($model), $this->redirect(\Yii::$app->request->referrer), 'error');
+        }
+        return $this->message('操作成功', $this->redirect(\Yii::$app->request->referrer), 'success');
+    }
+
+    /**
+     * 供应商-审核
+     *
+     * @return mixed
+     */
+    public function actionAjaxAudit()
+    {
+        $id = Yii::$app->request->get('id');
+        $model = $this->findModel($id);
+        $model = $model ?? new StyleStoneStyle();
+        // ajax 校验
+        $this->activeFormValidate($model);
+        if ($model->load(Yii::$app->request->post())) {
+            try{
+                $trans = Yii::$app->trans->beginTransaction();
+                if($model->audit_status == AuditStatusEnum::PASS){
+                    $model->auditor_id = \Yii::$app->user->id;
+                    $model->audit_time = time();
+                    $model->status = StatusEnum::ENABLED;
+                }else{
+                    $model->status = StatusEnum::DISABLED;
+                    $model->audit_status = AuditStatusEnum::SAVE;
+                }
+                if(false === $model->save()) {
+                    throw new \Exception($this->getError($model));
+                }
+                $trans->commit();
+            }catch (\Exception $e){
+                $trans->rollBack();
+                return $this->message("审核失败:". $e->getMessage(),  $this->redirect(Yii::$app->request->referrer), 'error');
+            }
+            return $this->message("保存成功", $this->redirect(Yii::$app->request->referrer), 'success');
+        }
+        $model->audit_status  = AuditStatusEnum::PASS;
+        return $this->renderAjax($this->action->id, [
+            'model' => $model,
+        ]);
+    }
+}
