64fc2d1d0735e5ed603fc2f498918ec690ac02dd
hexiaowei <xiaowei.he@bddco.com>
2020-07-13 20:02:50 +0800

    fix

--- addons/Sales/backend/controllers/DistributionOrderController.php
+++ addons/Sales/backend/controllers/DistributionOrderController.php
@@ -2,6 +2,7 @@
 
 namespace addons\Sales\backend\controllers;
 
+use addons\Sales\common\models\OrderGoods;
 use Yii;
 use common\traits\Curd;
 use common\models\base\SearchModel;
@@ -55,6 +56,41 @@ class DistributionOrderController extends BaseController
                 //'tabList'=>\Yii::$app->salesService->order->menuTabList($order_id,$this->returnUrl),
         ]);
     }
-    
-    
+
+    /**
+     * 详情展示页
+     * @return string
+     * @throws NotFoundHttpException
+     */
+    public function actionView()
+    {
+        $id = Yii::$app->request->get('id');
+        $tab = Yii::$app->request->get('tab',1);
+        $model = $this->findModel($id);
+
+        $dataProvider = null;
+        if (!is_null($id)) {
+            $searchModel = new SearchModel([
+                'model' => OrderGoods::class,
+                'scenario' => 'default',
+                'partialMatchAttributes' => [], // 模糊查询
+                'defaultOrder' => [
+                    'id' => SORT_DESC
+                ],
+                'pageSize' => 1000,
+            ]);
+
+            $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+            $dataProvider->query->andWhere(['=', 'order_id', $id]);
+
+            $dataProvider->setSort(false);
+        }
+        return $this->render($this->action->id, [
+            'model' => $model,
+            'dataProvider' => $dataProvider,
+            'returnUrl'=>$this->returnUrl,
+            'tab'=>$tab,
+        ]);
+    }
 }
