c3e1964eb2d81da442d5d042d093e0719656174e
hexiaowei <xiaowei.he@bddco.com>
2020-07-13 17:48:54 +0800

    fix

--- addons/Sales/backend/views/distribution-order/index.php
+++ addons/Sales/backend/views/distribution-order/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
-$this->title = '订单列表';
+$this->title = '待配货订单';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
