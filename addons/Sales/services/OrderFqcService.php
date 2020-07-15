f83c5122edab5655e3f80cc35dcd58a4a9d84562
hexiaowei <xiaowei.he@bddco.com>
2020-07-15 11:33:48 +0800

    fix

---
+++ addons/Sales/services/OrderFqcService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace addons\Sales\services;
+
+use Yii;
+use common\components\Service;
+
+/**
+ * Class OrderFqcService
+ * @package services\common
+ */
+class OrderFqcService extends Service
+{
+
+}
