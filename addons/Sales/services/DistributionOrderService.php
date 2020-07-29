e9d0e38a70b0773620aac2f3efa516135ca4c8a1
hexiaowei <xiaowei.he@bddco.com>
2020-07-29 19:26:12 +0800

    fix

--- addons/Sales/services/DistributionOrderService.php
+++ addons/Sales/services/DistributionOrderService.php
@@ -58,9 +58,9 @@ class DistributionOrderService extends Service
             if(!$goods){
                 throw new \Exception("货号".$goods_id."不存在");
             }
-            if($goods->goods_status != GoodsStatusEnum::IN_STOCK){
-                throw new \Exception("货号".$goods_id."不是库存状态");
-            }
+            //if($goods->goods_status != GoodsStatusEnum::IN_STOCK){
+                //throw new \Exception("货号".$goods_id."不是库存状态");
+            //}
             //if($goods->order_sn && $goods->order_detail_id){
                 //throw new \Exception("货号".$goods_id."已经被订单".$goods->order_sn."绑定");
             //}
@@ -120,11 +120,11 @@ class DistributionOrderService extends Service
         \Yii::$app->warehouseService->billS->createBillS($bill, $bill_goods);
 
         //2.更新商品库存状态
-        $condition = ['goods_id'=>$form->goods_ids, 'goods_status' => GoodsStatusEnum::IN_STOCK];
+        /*$condition = ['goods_id'=>$form->goods_ids, 'goods_status' => GoodsStatusEnum::IN_STOCK];
         $execute_num = WarehouseGoods::updateAll(['goods_status'=> GoodsStatusEnum::IN_SALE], $condition);
         if($execute_num <> count($bill_goods)){
             throw new Exception("货品改变状态数量与明细数量不一致");
-        }
+        }*/
         //3.更新订单配货状态
         $form->distribute_status = DistributeStatusEnum::HAS_PEIHUO;
         if(false === $form->save()){
