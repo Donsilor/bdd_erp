dffb420d96c1dc7c98c9aef20e7916045d1f3dbd
gaopeng <763429951@qq.com>
2020-08-05 16:51:33 +0800

    Signed-off-by: gaopeng <763429951@qq.com>

--- addons/Sales/backend/controllers/CustomerController.php
+++ addons/Sales/backend/controllers/CustomerController.php
@@ -92,7 +92,6 @@ class CustomerController extends BaseController
                 if($model->birthday){
                     $model->age = DateHelper::getYearByDate($model->birthday);
                 }
-                $this->creator_id =  Yii::$app->user->identity->getId();
                 if(false === $model->save()) {
                     throw new \Exception($this->getError($model));
                 }
