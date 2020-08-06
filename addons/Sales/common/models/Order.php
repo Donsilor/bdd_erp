c7da3d423d9cf034c3dda8aeb6f798cab0966d69
gaopeng <763429951@qq.com>
2020-08-06 20:28:27 +0800

    Signed-off-by: gaopeng <763429951@qq.com>

--- addons/Sales/common/models/Order.php
+++ addons/Sales/common/models/Order.php
@@ -145,7 +145,24 @@ class Order extends BaseModel
             'updated_at' => '更新时间',
         ];
     }
-    
+    /**
+     * @param bool $insert
+     * @return bool
+     */
+    public function beforeSave($insert)
+    {
+        if ($this->isNewRecord) {
+            if(isset(Yii::$app->user)) {
+                $this->creator_id = Yii::$app->user->identity->getId();
+                if(!$this->order_time) {
+                    $this->order_time = time();
+                }
+            }else{
+                $this->creator_id = 0;
+            }
+        }
+        return parent::beforeSave($insert);
+    }
     /**
      * 对应订单付款信息模型
      * @return \yii\db\ActiveQuery
