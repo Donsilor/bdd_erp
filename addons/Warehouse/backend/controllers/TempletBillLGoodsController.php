d2799a973f114ae21fbed6c76e905da904a60bed
hexiaowei <xiaowei.he@bddco.com>
2020-08-10 18:08:35 +0800

    fix

--- addons/Warehouse/backend/controllers/TempletBillLGoodsController.php
+++ addons/Warehouse/backend/controllers/TempletBillLGoodsController.php
@@ -2,16 +2,14 @@
 
 namespace addons\Warehouse\backend\controllers;
 
-use addons\Warehouse\common\enums\TempletStatusEnum;
-use addons\Warehouse\common\models\WarehouseTemplet;
-use addons\Warehouse\common\models\WarehouseTempletBill;
-use addons\Warehouse\common\models\WarehouseTempletBillGoods;
 use Yii;
 use common\traits\Curd;
 use common\models\base\SearchModel;
-use addons\Warehouse\common\models\WarehouseGoldBill;
-use addons\Warehouse\common\models\WarehouseGoldBillGoods;
+use addons\Warehouse\common\models\WarehouseTemplet;
+use addons\Warehouse\common\models\WarehouseTempletBill;
+use addons\Warehouse\common\models\WarehouseTempletBillGoods;
 use addons\Warehouse\common\forms\WarehouseTempletBillLGoodsForm;
+use addons\Warehouse\common\enums\TempletStatusEnum;
 use addons\Warehouse\common\enums\TempletBillTypeEnum;
 use common\helpers\Url;
 use common\helpers\ExcelHelper;
@@ -24,15 +22,17 @@ class TempletBillLGoodsController extends TempletBillGoodsController
     use Curd;
     public $modelClass = WarehouseTempletBillLGoodsForm::class;
     public $billType = TempletBillTypeEnum::TEMPLET_L;
+
     /**
      * Lists all StyleChannel models.
      * @return mixed
+     * @throws
      */
     public function actionIndex()
     {
         $bill_id = Yii::$app->request->get('bill_id');
-        $tab = Yii::$app->request->get('tab',2);
-        $returnUrl = Yii::$app->request->get('returnUrl',Url::to(['templet-bill-l-goods/index', 'bill_id'=>$bill_id]));
+        $tab = Yii::$app->request->get('tab', 2);
+        $returnUrl = Yii::$app->request->get('returnUrl', Url::to(['templet-bill-l-goods/index', 'bill_id' => $bill_id]));
         $searchModel = new SearchModel([
             'model' => $this->modelClass,
             'scenario' => 'default',
@@ -47,28 +47,28 @@ class TempletBillLGoodsController extends TempletBillGoodsController
         ]);
 
         $dataProvider = $searchModel
-            ->search(Yii::$app->request->queryParams,['created_at']);
+            ->search(Yii::$app->request->queryParams, ['created_at']);
 
         $created_at = $searchModel->created_at;
         if (!empty($created_at)) {
-            $dataProvider->query->andFilterWhere(['>=',WarehouseTempletBillGoods::tableName().'.created_at', strtotime(explode('/', $created_at)[0])]);//起始时间
-            $dataProvider->query->andFilterWhere(['<',WarehouseTempletBillGoods::tableName().'.created_at', (strtotime(explode('/', $created_at)[1]) + 86400)] );//结束时间
+            $dataProvider->query->andFilterWhere(['>=', WarehouseTempletBillGoods::tableName() . '.created_at', strtotime(explode('/', $created_at)[0])]);//起始时间
+            $dataProvider->query->andFilterWhere(['<', WarehouseTempletBillGoods::tableName() . '.created_at', (strtotime(explode('/', $created_at)[1]) + 86400)]);//结束时间
         }
 
         $dataProvider->query->andWhere(['=', 'bill_id', $bill_id]);
-        $dataProvider->query->andWhere(['>',WarehouseTempletBillGoods::tableName().'.status',-1]);
+        $dataProvider->query->andWhere(['>', WarehouseTempletBillGoods::tableName() . '.status', -1]);
 
         //导出
-        if(Yii::$app->request->get('action') === 'export'){
+        if (Yii::$app->request->get('action') === 'export') {
             $this->getExport($dataProvider);
         }
-        $bill = WarehouseTempletBill::find()->where(['id'=>$bill_id])->one();
+        $bill = WarehouseTempletBill::find()->where(['id' => $bill_id])->one();
         return $this->render($this->action->id, [
             'dataProvider' => $dataProvider,
             'searchModel' => $searchModel,
             'bill' => $bill,
             'tab' => $tab,
-            'tabList'=>\Yii::$app->warehouseService->templetBill->menuTabList($bill_id, $this->billType, $returnUrl),
+            'tabList' => \Yii::$app->warehouseService->templetBill->menuTabList($bill_id, $this->billType, $returnUrl),
         ]);
     }
 
@@ -88,19 +88,19 @@ class TempletBillLGoodsController extends TempletBillGoodsController
         // ajax 校验
         $this->activeFormValidate($model);
         if ($model->load(\Yii::$app->request->post())) {
-            try{
+            try {
                 $trans = \Yii::$app->db->beginTransaction();
                 $model->bill_id = $bill_id;
                 $model->bill_no = $bill->bill_no;
                 $model->bill_type = $bill->bill_type;
                 $model->cost_price = bcmul($model->cost_price, $model->goods_weight, 3);
-                if(false === $model->save()) {
+                if (false === $model->save()) {
                     throw new \Exception($this->getError($model));
                 }
                 \Yii::$app->warehouseService->templetBill->BillSummary($model->bill_id);
                 $trans->commit();
-                return $this->message('保存成功',$this->redirect(Yii::$app->request->referrer),'success');
-            }catch (\Exception $e){
+                return $this->message('保存成功', $this->redirect(Yii::$app->request->referrer), 'success');
+            } catch (\Exception $e) {
                 $trans->rollBack();
                 return $this->message($e->getMessage(), $this->redirect(\Yii::$app->request->referrer), 'error');
             }
@@ -119,8 +119,8 @@ class TempletBillLGoodsController extends TempletBillGoodsController
     public function actionEditAll()
     {
         $bill_id = Yii::$app->request->get('bill_id');
-        $tab = Yii::$app->request->get('tab',3);
-        $returnUrl = Yii::$app->request->get('returnUrl',Url::to(['templet-bill-l-goods/index', 'bill_id' => $bill_id]));
+        $tab = Yii::$app->request->get('tab', 3);
+        $returnUrl = Yii::$app->request->get('returnUrl', Url::to(['templet-bill-l-goods/index', 'bill_id' => $bill_id]));
         $searchModel = new SearchModel([
             'model' => $this->modelClass,
             'scenario' => 'default',
@@ -137,16 +137,16 @@ class TempletBillLGoodsController extends TempletBillGoodsController
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         $dataProvider->query->andWhere(['=', 'bill_id', $bill_id]);
-        $dataProvider->query->andWhere(['>',WarehouseTempletBillGoods::tableName().'.status',-1]);
+        $dataProvider->query->andWhere(['>', WarehouseTempletBillGoods::tableName() . '.status', -1]);
 
-        $bill = WarehouseTempletBill::find()->where(['id'=>$bill_id])->one();
+        $bill = WarehouseTempletBill::find()->where(['id' => $bill_id])->one();
         return $this->render($this->action->id, [
             'dataProvider' => $dataProvider,
             'searchModel' => $searchModel,
             'bill' => $bill,
             'tabList' => \Yii::$app->warehouseService->templetBill->menuTabList($bill_id, $this->billType, $returnUrl, $tab),
             'returnUrl' => $returnUrl,
-            'tab'=>$tab,
+            'tab' => $tab,
         ]);
     }
 
@@ -165,14 +165,14 @@ class TempletBillLGoodsController extends TempletBillGoodsController
             $trans = Yii::$app->trans->beginTransaction();
 
             $model = $this->findModel($id) ?? new WarehouseTempletBillGoods();
-            WarehouseTemplet::updateAll(['goods_status'=>TempletStatusEnum::IN_STOCK], ['batch_sn'=>$model->batch_sn]);
+            WarehouseTemplet::updateAll(['goods_status' => TempletStatusEnum::IN_STOCK], ['batch_sn' => $model->batch_sn]);
             if (!$model->delete()) {
                 throw new \Exception("删除失败", 422);
             }
             //更新单据汇总
             Yii::$app->warehouseService->templetBill->BillSummary($purchase_id);
             $trans->commit();
-            return $this->message('删除成功',$this->redirect(Yii::$app->request->referrer),'success');
+            return $this->message('删除成功', $this->redirect(Yii::$app->request->referrer), 'success');
         } catch (\Exception $e) {
             $trans->rollback();
             return $this->message($e->getMessage(), $this->redirect(Yii::$app->request->referrer), 'error');
