4816d41e128f33e286bd88df481cd415bd2cc392
gaopeng <763429951@qq.com>
2020-08-26 18:25:39 +0800

    Signed-off-by: gaopeng <763429951@qq.com>

--- addons/Sales/common/enums/OrderFromEnum.php
+++ addons/Sales/common/enums/OrderFromEnum.php
@@ -10,7 +10,7 @@ class OrderFromEnum extends \common\enums\BaseEnum
 {
     const FROM_HAND = 1;
     const FROM_BDD = 2;
-    const FROM_JD = 2;
+    const FROM_JD = 3;
     /**
      * @return array
      */
@@ -19,7 +19,7 @@ class OrderFromEnum extends \common\enums\BaseEnum
         return [
                 self::FROM_HAND => "手动创建",
                 self::FROM_BDD => "BDD同步",
-                self::FROM_BDD => "JD同步",
+                self::FROM_JD => "JD同步",
         ];
     }
     
