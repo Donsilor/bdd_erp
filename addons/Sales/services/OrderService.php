772d299d10a9b1c61d2de055d2832e6328bb600c
gaopeng <763429951@qq.com>
2020-08-27 13:02:31 +0800

    Signed-off-by: gaopeng <763429951@qq.com>

--- addons/Sales/services/OrderService.php
+++ addons/Sales/services/OrderService.php
@@ -270,7 +270,9 @@ class OrderService extends Service
         $invoice->attributes = $invoiceInfo;
         if(false == $invoice->save()) {
             throw new \Exception("同步发票失败：".$this->getError($invoice));
-        } 
+        } else{
+            echo $invoice->toArray();
+        }
         $order->is_invoice   = $invoice->invoice_type ? 1: 0;
         $order->goods_num   = $goods_num;
         $order->customer_id = $customer->id;
