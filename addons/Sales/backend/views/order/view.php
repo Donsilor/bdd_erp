2d29fb11e5c200a359d2aabcccb6fc8e6e246079
hexiaowei <xiaowei.he@bddco.com>
2020-08-31 14:02:53 +0800

    fix

--- addons/Sales/backend/views/order/view.php
+++ addons/Sales/backend/views/order/view.php
@@ -163,7 +163,8 @@ $this->params['breadcrumbs'][] = $this->title;
                     }
                     ?>
                     <?php
-                    if ($model->pay_status == \addons\Sales\common\enums\PayStatusEnum::HAS_PAY) {
+                    if ($model->pay_status == \addons\Sales\common\enums\PayStatusEnum::HAS_PAY
+                        && $model->refund_status != \addons\Sales\common\enums\RefundStatusEnum::HAS_RETURN) {
                         echo Html::edit(['return', 'id' => $model->id], '退款', [
                             //'data-toggle' => 'modal',
                             'class' => 'btn btn-warning btn-ms openIframe',
