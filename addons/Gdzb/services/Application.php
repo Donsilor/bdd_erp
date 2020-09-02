ad2909fa0a957c3f89db4228a326cf96b4540a11
lrz0829 <502034356@qq.com>
2020-09-02 20:08:20 +0800

    1

--- addons/Gdzb/services/Application.php
+++ addons/Gdzb/services/Application.php
@@ -9,6 +9,8 @@ use common\components\Service;
  *
  * @package addons\Gdzb\services
  * @property \addons\Gdzb\services\OrderService $order 订单
+ * @property \addons\Gdzb\services\SupplierService $supplier 供应商
+ * @property \addons\Gdzb\services\CustomerService $customer 客户
  * @var array
  */
 class Application extends Service
@@ -17,5 +19,7 @@ class Application extends Service
     public $childService = [
             /*********订单相关*********/
             'order' => 'addons\Gdzb\services\OrderService',            
+            'supplier' => 'addons\Gdzb\services\SupplierService',
+            'customer' => 'addons\Gdzb\services\CustomerService',
     ];
 }
