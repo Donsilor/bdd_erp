d0867cb29c84cd1b017586edfd3e1a83bc5d4ec1
hexiaowei <xiaowei.he@bddco.com>
2020-09-08 09:59:51 +0800

    fix

--- addons/Style/common/forms/StyleForm.php
+++ addons/Style/common/forms/StyleForm.php
@@ -54,19 +54,19 @@ class StyleForm extends Style
             $this->getAttributeLabel('is_gift') . $this->formatTitleId($this->getIsGiftList()),
             '#',
 
-            $this->formatTitleId($this->getSupplierList()),
+            "工厂名称1" . $this->formatTitleId($this->getSupplierList()),
             '#', '#', '#',
-            $this->formatTitleId($this->getIsMadeList()),
+            "是否支持定制1" . $this->formatTitleId($this->getIsMadeList()),
 
-            $this->formatTitleId($this->getSupplierList()),
+            "工厂名称2" . $this->formatTitleId($this->getSupplierList()),
             '#', '#', '#',
-            $this->formatTitleId($this->getIsMadeList()),
+            "是否支持定制2" . $this->formatTitleId($this->getIsMadeList()),
 
             '#', '#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#',
         ];
         $fields = [
             '(*)款式名称', '款式编号', '(*)款式分类', '(*)产品线', '(*)归属渠道', '款式来源', '(*)款式材质', '(*)款式性别', '是否支持定制', '是否赠品', '备注',
-            '工厂名称1', '工厂模号1', '备注(计费方式)1', '出货时间(天)1', '是否支持定制1',
+            '工厂名称1(默认工厂)', '工厂模号1', '备注(计费方式)1', '出货时间(天)1', '是否支持定制1',
             '工厂名称2', '工厂模号2', '备注(计费方式)2', '出货时间(天)2', '是否支持定制2',
             '配石工费/ct', '配件工费', '克/工费', '基本工费', '镶石费', '表面工艺费', '分色费', '喷拉沙费', '补口费', '版费', '证书费', '其他费用',
         ];
