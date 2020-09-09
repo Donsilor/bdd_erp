6237edf9512281e8b20b48f14289087fa1fd8814
gaopeng <763429951@qq.com>
2020-09-09 17:13:44 +0800

    Signed-off-by: gaopeng <763429951@qq.com>

--- addons/Warehouse/backend/views/bill-c-goods/index.php
+++ addons/Warehouse/backend/views/bill-c-goods/index.php
@@ -40,9 +40,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="tab-content">
         <div class="row col-xs-12">
             <div class="box">
-                <div class="box-header">
-                    <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
-                </div>
                 <div class="box-body table-responsive">
                     <?php echo Html::batchButtons(false)?>
                     <?= GridView::widget([
