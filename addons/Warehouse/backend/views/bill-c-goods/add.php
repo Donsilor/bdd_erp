8f9d60f18b3d2d8ecbd802c64f233795072f4813
hexiaowei <xiaowei.he@bddco.com>
2020-09-25 12:23:49 +0800

    fix

--- addons/Warehouse/backend/views/bill-c-goods/add.php
+++ addons/Warehouse/backend/views/bill-c-goods/add.php
@@ -17,231 +17,264 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="box">
             <?php $form = ActiveForm::begin([]); ?>
             <div class="box-body" style="padding:20px 50px">
-                 <?= $form->field($model, 'id')->hiddenInput()->label(false) ?>
-                 <div class="row">
-                     <div class="col-lg-4">
-                        <?= $form->field($model, 'goods_ids')->textarea(["placeholder"=>"请输入货号，多个请用用逗号/空格/换行符隔开",'style'=>'height:100px']) ?>
-                     </div>
-                     <div class="col-lg-1">
-                        <?= Html::button('查询',['class'=>'btn btn-info btn-sm','style'=>'margin-top:27px;','onclick'=>"searchWarehouseGoods()"]) ?>
-                     </div>
-                 </div>
+                <?= $form->field($model, 'id')->hiddenInput()->label(false) ?>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <?= $form->field($model, 'goods_ids')->textarea(["placeholder" => "请输入货号，多个请用用逗号/空格/换行符隔开", 'style' => 'height:100px']) ?>
+                    </div>
+                    <div class="col-lg-1">
+                        <?= Html::button('查询', ['class' => 'btn btn-info btn-sm', 'style' => 'margin-top:27px;', 'onclick' => "searchGoods()"]) ?>
+                    </div>
+                    <div class="col-sm-6">
+                        <div>
+                            <label class="control-label" style="vertical-align:top">
+                                操作日志提示
+                            </label>
+                        </div>
+                        <div id="search_logs" title="系统日志"
+                             style="width:100%; height:120px; padding:5px; color:red; border:1px solid #cecece;overflow:scroll">
+                            <?= $message?>
+                        </div>
+                    </div>
+                </div>
                 <div class="box-body table-responsive">
                     <div class="tab-content">
                         <?php
                         $warehouseColomns = [
                             [
-                                'name' =>'goods_id',
-                                'title'=>"货号",
-                                'enableError'=>false,
+                                'name' => 'goods_id',
+                                'title' => "货号",
+                                'enableError' => false,
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'readonly' =>'true',
-                                    'style'=>'width:160px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:160px'
                                 ]
                             ],
                             [
-                                'name' =>'style_sn',
-                                'title'=>"款号",
-                                'enableError'=>false,
+                                'name' => 'style_sn',
+                                'title' => "款号",
+                                'enableError' => false,
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'readonly' =>'true',
-                                    'style'=>'width:160px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:160px'
                                 ]
                             ],
                             [
-                                'name' =>'goods_name',
-                                'title'=>"商品名称",
-                                'enableError'=>false,
+                                'name' => 'goods_name',
+                                'title' => "商品名称",
+                                'enableError' => false,
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'readonly' =>'true',
-                                    'style'=>'width:120px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:120px'
                                 ]
                             ],
 
                             [
-                                'name' =>'goods_num',
-                                'title'=>"商品数量",
-                                'enableError'=>false,
+                                'name' => 'goods_num',
+                                'title' => "商品数量",
+                                'enableError' => false,
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'readonly' =>'true',
-                                    'style'=>'width:60px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:60px'
                                 ]
                             ],
                             [
                                 'name' => "put_in_type",
-                                'title'=>"入库方式",
-                                'enableError'=>false,
-                                'type'  => 'dropDownList',
+                                'title' => "入库方式",
+                                'enableError' => false,
+                                'type' => 'dropDownList',
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'disabled' =>'true',
-                                    'style'=>'width:120px',
-                                    'prompt'=>'请选择',
+                                    'disabled' => 'true',
+                                    'style' => 'width:120px',
+                                    'prompt' => '请选择',
                                 ],
                                 'items' => \addons\Warehouse\common\enums\PutInTypeEnum::getMap()
                             ],
                             [
                                 'name' => "warehouse_id",
-                                'title'=>"仓库",
-                                'enableError'=>false,
-                                'type'  => 'dropDownList',
+                                'title' => "仓库",
+                                'enableError' => false,
+                                'type' => 'dropDownList',
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'disabled' =>'true',
-                                    'style'=>'width:120px',
-                                    'prompt'=>'请选择',
+                                    'disabled' => 'true',
+                                    'style' => 'width:120px',
+                                    'prompt' => '请选择',
                                 ],
                                 'items' => Yii::$app->warehouseService->warehouse::getDropDown()
                             ],
                             [
                                 'name' => "material",
-                                'title'=>"主成色",
-                                'enableError'=>false,
-                                'type'  => 'dropDownList',
+                                'title' => "主成色",
+                                'enableError' => false,
+                                'type' => 'dropDownList',
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'disabled' =>'true',
-                                    'style'=>'width:120px',
-                                    'prompt'=>'请选择',
+                                    'disabled' => 'true',
+                                    'style' => 'width:120px',
+                                    'prompt' => '请选择',
                                 ],
                                 'defaultValue' => 0,
                                 'items' => \Yii::$app->attr->valueMap(\addons\Purchase\common\enums\ReceiptGoodsAttrEnum::MATERIAL)
                             ],
                             [
                                 'name' => "gold_weight",
-                                'title'=>"金重",
-                                'enableError'=>false,
+                                'title' => "金重",
+                                'enableError' => false,
                                 'defaultValue' => 0,
                                 'options' => [
                                     'class' => 'input-priority',
                                     'type' => 'number',
-                                    'readonly' =>'true',
-                                    'style'=>'width:80px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:80px'
                                 ]
                             ],
                             [
                                 'name' => "gold_loss",
-                                'title'=>"金损",
-                                'enableError'=>false,
+                                'title' => "金损",
+                                'enableError' => false,
                                 'defaultValue' => 0,
                                 'options' => [
                                     'class' => 'input-priority',
                                     'type' => 'number',
-                                    'readonly' =>'true',
-                                    'style'=>'width:80px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:80px'
                                 ]
                             ],
                             [
                                 'name' => "diamond_carat",
-                                'title'=>"钻石大小",
-                                'enableError'=>false,
+                                'title' => "钻石大小",
+                                'enableError' => false,
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'readonly' =>'true',
-                                    'style'=>'width:80px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:80px'
                                 ]
                             ],
                             [
                                 'name' => "diamond_color",
-                                'title'=>"钻石颜色",
-                                'enableError'=>false,
-                                'type'  => 'dropDownList',
+                                'title' => "钻石颜色",
+                                'enableError' => false,
+                                'type' => 'dropDownList',
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'disabled' =>'true',
-                                    'style'=>'width:100px',
-                                    'prompt'=>'请选择',
+                                    'disabled' => 'true',
+                                    'style' => 'width:100px',
+                                    'prompt' => '请选择',
                                 ],
                                 'items' => \Yii::$app->attr->valueMap(\addons\Purchase\common\enums\ReceiptGoodsAttrEnum::MAIN_STONE_COLOR)
                             ],
                             [
                                 'name' => "diamond_clarity",
-                                'title'=>"钻石净度",
-                                'enableError'=>false,
-                                'type'  => 'dropDownList',
+                                'title' => "钻石净度",
+                                'enableError' => false,
+                                'type' => 'dropDownList',
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'disabled' =>'true',
-                                    'style'=>'width:100px',
-                                    'prompt'=>'请选择',
+                                    'disabled' => 'true',
+                                    'style' => 'width:100px',
+                                    'prompt' => '请选择',
                                 ],
                                 'items' => \Yii::$app->attr->valueMap(\addons\Purchase\common\enums\ReceiptGoodsAttrEnum::MAIN_STONE_CLARITY)
                             ],
                             [
                                 'name' => "diamond_cert_id",
-                                'title'=>"证书号",
-                                'enableError'=>false,
+                                'title' => "证书号",
+                                'enableError' => false,
                                 'options' => [
                                     'class' => 'input-priority',
-                                    'readonly' =>'true',
-                                    'style'=>'width:80px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:80px'
                                 ]
                             ],
                             [
                                 'name' => "cost_price",
-                                'title'=>"成本价",
-                                'enableError'=>false,
+                                'title' => "成本价",
+                                'enableError' => false,
                                 'defaultValue' => 0,
                                 'options' => [
                                     'class' => 'input-priority',
                                     'type' => 'number',
-                                    'readonly' =>'true',
-                                    'style'=>'width:80px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:80px'
                                 ]
                             ],
                             [
                                 'name' => "sale_price",
-                                'title'=>"销售价",
-                                'enableError'=>false,
+                                'title' => "销售价",
+                                'enableError' => false,
                                 'defaultValue' => 0,
                                 'options' => [
                                     'class' => 'input-priority',
                                     'type' => 'number',
-                                    'readonly' =>'true',
-                                    'style'=>'width:80px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:80px'
                                 ]
                             ],
                             [
                                 'name' => "market_price",
-                                'title'=>"市场价",
-                                'enableError'=>false,
+                                'title' => "市场价",
+                                'enableError' => false,
                                 'defaultValue' => 0,
                                 'options' => [
                                     'class' => 'input-priority',
                                     'type' => 'number',
-                                    'readonly' =>'true',
-                                    'style'=>'width:80px'
+                                    'readonly' => 'true',
+                                    'style' => 'width:80px'
                                 ]
                             ]
                         ];
                         ?>
                         <?= unclead\multipleinput\MultipleInput::widget([
                             'name' => "bill_goods",
-                            'addButtonOptions'=>['label'=>'','class'=>''],
-                            'value' => $warehouse_goods,
+                            'addButtonOptions' => ['label' => '', 'class' => ''],
+                            'value' => $searchGoods,
                             'columns' => $warehouseColomns,
                         ]) ?>
                     </div>
                 </div>
-               <!-- ./box-body -->
+                <!-- ./box-body -->
             </div>
             <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
 <script type="text/javascript">
-    function searchWarehouseGoods() {
-        var goods_ids = $("#warehousebillcform-goods_ids").val();
-        if(!goods_ids) {
+    var formId = 'warehousebillcform';
+
+    function searchGoods() {
+        var goods_ids = $("#" + formId + "-goods_ids").val();
+        if (!goods_ids) {
             rfMsg("请输入货号");
             return false;
         }
-        goods_ids = goods_ids.replace(/\n/g,',');
-        var url = "<?= Url::buildUrl(\Yii::$app->request->url,[],['goods_ids','search',])?>&search=1&goods_ids="+goods_ids;
-        window.location.href = url;
+        goods_ids = goods_ids.replace(/\n/g, ',');
+        $.ajax({
+            type: "get",
+            url: '<?= Url::buildUrl(\Yii::$app->request->url)?>',
+            dataType: "json",
+            data: {
+                'search': 1,
+                'goods_ids': goods_ids,
+            },
+            success: function (data) {
+                if (parseInt(data.code) == 200) {
+                    var url = "<?= Url::buildUrl(\Yii::$app->request->url, [], ['goods_ids', 'message'])?>&goods_ids=" + data.data.valid_goods_ids + "&message=" + data.data.message;
+                    console.log(url);
+                    window.location.href = url;
+                }
+                //if (parseInt(data.code) == 200) {
+                //    //$('#search_logs').html(data.message);//日志显示
+                //    window.location.href = "<?//= Url::buildUrl(\Yii::$app->request->url,[],['goods_ids', 'message'])?>//&goods_ids="+data.goods_ids+"message="+data.message;
+                //}else{
+                //    $('#search_logs').html(data.message);//日志显示
+                //}
+            }
+        });
     }
 </script>
