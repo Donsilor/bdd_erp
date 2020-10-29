7fd692d6c2e72ad0647f6db5b733fbd11bf814bf
hexiaowei <xiaowei.he@bddco.com>
2020-10-29 18:47:06 +0800

    fix

--- backend/views/main/system.php
+++ backend/views/main/system.php
@@ -308,37 +308,40 @@ $this->params['breadcrumbs'][] = ['label' => $this->title];
                         </a>
                     </div>
                     <div class="pending-content">
-                        <?php foreach ($pend['list'] ?? [] as $time => $list) { ?>
-                            <div class="pending-list">
-                                <div class="date-box">
-                                    <div class="date"><?= $time ?? "" ?></div>
-                                    <div class="date date-start"></div>
-                                </div>
-                                <div class="order-box">
-                                    <div>
-                                        <?php foreach ($list ?? [] as $p) { ?>
-                                            <div class="order-child <?php if ($p['pend_status'] ?? 0) {
-                                                echo 'finish';
-                                            } ?>">
-                                                <div class="order-l">
-                                                    <div class="order-date"><?= date('Y/m/d H:i', $p['created_at'] ?? 0) ?? "" ?></div>
-                                                    <div class="order-text">
-                                                        <?= '[' . \common\enums\OperTypeEnum::getValue($p['oper_type'] ?? "") . ']：' ?? "" ?>
-                                                        <span><a class="openContab" style="text-decoration:underline;"
-                                                                 href="<?= Url::buildUrl('../' . \common\enums\OperTypeEnum::getUrlValue($p['oper_type']), [], ['id']) . '?id=' . $p['oper_id']; ?>"><?= $p['oper_sn'] ?? "" ?></a></span>，需审核请及时处理！
+                        <?php foreach ($pend['list'] ?? [] as $time => $list) {
+                            if ($list && is_array($list)) { ?>
+                                <div class="pending-list">
+                                    <div class="date-box">
+                                        <div class="date"><?= $time ?? "" ?></div>
+                                        <div class="date date-start"></div>
+                                    </div>
+                                    <div class="order-box">
+                                        <div>
+                                            <?php foreach ($list ?? [] as $p) { ?>
+                                                <div class="order-child <?php if ($p['pend_status'] ?? 0) {
+                                                    echo 'finish';
+                                                } ?>">
+                                                    <div class="order-l">
+                                                        <div class="order-date"><?= date('Y/m/d H:i', $p['created_at'] ?? 0) ?? "" ?></div>
+                                                        <div class="order-text">
+                                                            <?= '[' . \common\enums\OperTypeEnum::getValue($p['oper_type'] ?? "") . ']：' ?? "" ?>
+                                                            <span><a class="openContab"
+                                                                     style="text-decoration:underline;"
+                                                                     href="<?= Url::buildUrl('../' . \common\enums\OperTypeEnum::getUrlValue($p['oper_type']), [], ['id']) . '?id=' . $p['oper_id']; ?>"><?= $p['oper_sn'] ?? "" ?></a></span>，需审核请及时处理！
+                                                        </div>
+                                                    </div>
+                                                    <div class="order-state">
+                                                        <?= '【' . \common\enums\PendStatusEnum::getValue($p['pend_status'] ?? 0) . "】" ?? "" ?>
                                                     </div>
                                                 </div>
-                                                <div class="order-state">
-                                                    <?= '【' . \common\enums\PendStatusEnum::getValue($p['pend_status'] ?? 0) . "】" ?? "" ?>
-                                                </div>
-                                            </div>
-                                        <?php } ?>
+                                            <?php } ?>
+                                        </div>
+                                        <div class="point"></div>
+                                        <div class="line"></div>
                                     </div>
-                                    <div class="point"></div>
-                                    <div class="line"></div>
                                 </div>
-                            </div>
-                        <?php } ?>
+                            <?php }
+                        } ?>
                     </div>
                 </div>
             </div>
@@ -353,32 +356,34 @@ $this->params['breadcrumbs'][] = ['label' => $this->title];
                     <div class="quick-box">
                         <?php foreach ($quick['list'] ?? [] as $title => $list) {
                             $urls = $quick['url'] ?? [];
-                            ?>
-                            <div class="quick-list clf">
-                                <div class="quick-nav fl"><?= $title; ?></div>
-                                <div class="quick-content fl">
-                                    <?php foreach ($list ?? [] as $id => $name) {
-                                        if ($urls) {
-                                            $url = Url::buildUrl('../' . $urls[$id] ?? "", [], []);
-                                            $authUrl = \common\helpers\Auth::verify('/'.$urls[$id]);
-                                        }
-                                        ?>
-                                        <?php if ($authUrl) { ?>
-                                            <a class="quick-child openContab"
-                                               href="<?= $url ?? 'javascript:;' ?>">
-                                                <div class="inline clf">
-                                                    <span class="quick-text fl"><?= $name ?? "" ?></span>
-                                                    <div class="more-icon fl">
-                                                        <span></span>
-                                                        <span></span>
+                            if ($list && is_array($list)) {
+                                ?>
+                                <div class="quick-list clf">
+                                    <div class="quick-nav fl"><?= $title; ?></div>
+                                    <div class="quick-content fl">
+                                        <?php foreach ($list ?? [] as $id => $name) {
+                                            if ($urls) {
+                                                $url = Url::buildUrl('../' . $urls[$id] ?? "", [], []);
+                                                $authUrl = \common\helpers\Auth::verify('/' . $urls[$id]);
+                                            }
+                                            ?>
+                                            <?php if ($authUrl) { ?>
+                                                <a class="quick-child openContab"
+                                                   href="<?= $url ?? 'javascript:;' ?>">
+                                                    <div class="inline clf">
+                                                        <span class="quick-text fl"><?= $name ?? "" ?></span>
+                                                        <div class="more-icon fl">
+                                                            <span></span>
+                                                            <span></span>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </a>
+                                                </a>
+                                            <?php } ?>
                                         <?php } ?>
-                                    <?php } ?>
+                                    </div>
                                 </div>
-                            </div>
-                        <?php } ?>
+                            <?php }
+                        } ?>
                     </div>
                 </div>
             </div>
