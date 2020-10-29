028cf024cb7c728d7cbf63dc0fc07a0ca664a4ee
hexiaowei <xiaowei.he@bddco.com>
2020-10-29 20:25:18 +0800

    fix

--- backend/modules/base/controllers/PendController.php
+++ backend/modules/base/controllers/PendController.php
@@ -65,4 +65,47 @@ class PendController extends BaseController
         ]);
     }
 
+    /**
+     * 个人首页
+     *
+     * @return string
+     * @throws \yii\web\NotFoundHttpException
+     */
+    public function actionPending()
+    {
+        $searchModel = new SearchModel([
+            'model' => $this->modelClass,
+            'scenario' => 'default',
+            'partialMatchAttributes' => [], // 模糊查询
+            'defaultOrder' => [
+                'id' => SORT_DESC
+            ],
+            'pageSize' => $this->pageSize
+        ]);
+
+        $dataProvider = $searchModel
+            ->search(Yii::$app->request->queryParams, ['created_at', 'pend_time']);
+        $created_at = $searchModel->created_at;
+        if (!empty($created_at)) {
+            $dataProvider->query->andFilterWhere(['>=', 'created_at', strtotime(explode('/', $created_at)[0])]);//起始时间
+            $dataProvider->query->andFilterWhere(['<', 'created_at', (strtotime(explode('/', $created_at)[1]) + 86400)]);//结束时间
+        }
+        $pend_time = $searchModel->pend_time;
+        if (!empty($pend_time)) {
+            $dataProvider->query->andFilterWhere(['>=', 'pend_time', strtotime(explode('/', $pend_time)[0])]);//起始时间
+            $dataProvider->query->andFilterWhere(['<', 'pend_time', (strtotime(explode('/', $pend_time)[1]) + 86400)]);//结束时间
+        }
+        $dataProvider->query
+            ->andWhere(['>=', 'status', StatusEnum::DISABLED]);
+        $uid = \Yii::$app->user->identity->getId();
+        if ($uid > 1) {
+            $dataProvider->query
+                ->andWhere(['=', 'operor_id', $uid]);
+        }
+        return $this->render($this->action->id, [
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
+        ]);
+    }
+
 }
